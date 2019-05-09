d5d069e74b7b5d806a922fae6c1a9f06bc09f25b
Julien Sagnard <julien.sagnard@rohde-schwarz.com>
2019-05-09 18:28:45 +0200

    Improve with PR comments

--- connexion/apis/flask_api.py
+++ connexion/apis/flask_api.py
@@ -26,13 +26,6 @@ class FlaskApi(AbstractAPI):
         self.blueprint = flask.Blueprint(endpoint, __name__, url_prefix=self.base_path,
                                          template_folder=str(self.options.openapi_console_ui_from_dir))
 
-    def _spec_for_prefix(self):
-        """ Modify base_path in the spec based on incoming url
-            This fixes problems with reverse proxies changing the path.
-        """
-        base_path = flask.url_for(flask.request.endpoint).rsplit("/", 1)[0]
-        return self.specification.with_base_path(base_path).raw
-
     def add_openapi_json(self):
         """
         Adds spec json to {base_path}/swagger.json
@@ -42,12 +35,9 @@ class FlaskApi(AbstractAPI):
                      self.options.openapi_spec_path)
         endpoint_name = "{name}_openapi_json".format(name=self.blueprint.name)
 
-        def get_json_spec():
-            return flask.jsonify(self._spec_for_prefix())
-
         self.blueprint.add_url_rule(self.options.openapi_spec_path,
                                     endpoint_name,
-                                    get_json_spec)
+                                    self._handlers.get_json_spec)
 
     def add_openapi_yaml(self):
         """
@@ -65,11 +55,7 @@ class FlaskApi(AbstractAPI):
         self.blueprint.add_url_rule(
             openapi_spec_path_yaml,
             endpoint_name,
-            lambda: FlaskApi._build_flask_response(
-                status_code=200,
-                content_type="text/yaml",
-                data=yamldumper(self._spec_for_prefix())
-            )
+            self._handlers.get_yaml_spec
         )
 
     def add_swagger_ui(self):
@@ -122,7 +108,7 @@ class FlaskApi(AbstractAPI):
     def _handlers(self):
         # type: () -> InternalHandlers
         if not hasattr(self, '_internal_handlers'):
-            self._internal_handlers = InternalHandlers(self.base_path, self.options)
+            self._internal_handlers = InternalHandlers(self.base_path, self.options, self.specification)
         return self._internal_handlers
 
     @classmethod
@@ -301,9 +287,10 @@ class InternalHandlers(object):
     Flask handlers for internally registered endpoints.
     """
 
-    def __init__(self, base_path, options):
+    def __init__(self, base_path, options, specification):
         self.base_path = base_path
         self.options = options
+        self.specification = specification
 
     def console_ui_home(self):
         """
@@ -312,7 +299,7 @@ class InternalHandlers(object):
         :return:
         """
         openapi_json_route_name = "{blueprint}.{prefix}_openapi_json"
-        escaped = self.base_path.replace(".", "_")
+        escaped = flask_utils.flaskify_endpoint(self.base_path)
         openapi_json_route_name = openapi_json_route_name.format(
             blueprint=escaped,
             prefix=escaped
@@ -333,3 +320,21 @@ class InternalHandlers(object):
         # convert PosixPath to str
         static_dir = str(self.options.openapi_console_ui_from_dir)
         return flask.send_from_directory(static_dir, filename)
+
+    def get_json_spec(self):
+        return flask.jsonify(self._spec_for_prefix())
+
+    def get_yaml_spec(self):
+        return FlaskApi._build_flask_response(
+            status_code=200,
+            content_type="text/yaml",
+            data=yamldumper(self._spec_for_prefix())
+        )
+
+    def _spec_for_prefix(self):
+        """
+        Modify base_path in the spec based on incoming url
+        This fixes problems with reverse proxies changing the path.
+        """
+        base_path = flask.url_for(flask.request.endpoint).rsplit("/", 1)[0]
+        return self.specification.with_base_path(base_path).raw
